d18670d56175b3337c9d221fc89ffb9429186146
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-05-31 08:37:24 +0530

    Add tabsToSpaces.

--- goatsay.go
+++ goatsay.go
@@ -7,21 +7,21 @@ import (
 
 func makeBubble(text []string, longest int) string {
 	var out []string
-	lines := len(text)
+	lineCount := len(text)
 	borders := []string{"/", "\\", "|", "(", ")"}
 	top := " " + strings.Repeat("_", longest+2)
 	bottom := " " + strings.Repeat("-", longest+2)
 
 	out = append(out, top)
 
-	if lines == 1 {
+	if lineCount == 1 {
 		s := fmt.Sprintf("%s %s %s", borders[3], text[0], borders[4])
 		out = append(out, s)
 	} else {
 		s := fmt.Sprintf(`%s %s %s`, borders[0], text[0], borders[1])
 		out = append(out, s)
 		i := 1
-		for ; i < lines-1; i++ {
+		for ; i < lineCount-1; i++ {
 			s = fmt.Sprintf(`%s %s %s`, borders[2], text[i], borders[2])
 			out = append(out, s)
 		}
@@ -31,4 +31,13 @@ func makeBubble(text []string, longest int) string {
 
 	out = append(out, bottom)
 	return strings.Join(out, "\n")
+}
+
+func tabsToSpaces(text []string) []string {
+	var out []string
+	for _, line := range text {
+		line = strings.ReplaceAll(line, "\t", "    ")
+		out = append(out, line)
+	}
+	return out
 }
