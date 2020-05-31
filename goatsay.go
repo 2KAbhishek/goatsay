ef7650b3580be66f1b3351aa57e58856155bcc34
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-05-31 08:27:44 +0530

    Add makeBubble

--- goatsay.go
+++ goatsay.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func makeBubble(text []string, longest int) string {
+	var out []string
+	lines := len(text)
+	borders := []string{"/", "\\", "|", "(", ")"}
+	top := " " + strings.Repeat("_", longest+2)
+	bottom := " " + strings.Repeat("-", longest+2)
+
+	out = append(out, top)
+
+	if lines == 1 {
+		s := fmt.Sprintf("%s %s %s", borders[3], text[0], borders[4])
+		out = append(out, s)
+	} else {
+		s := fmt.Sprintf(`%s %s %s`, borders[0], text[0], borders[1])
+		out = append(out, s)
+		i := 1
+		for ; i < lines-1; i++ {
+			s = fmt.Sprintf(`%s %s %s`, borders[2], text[i], borders[2])
+			out = append(out, s)
+		}
+		s = fmt.Sprintf(`%s %s %s`, borders[1], text[i], borders[0])
+		out = append(out, s)
+	}
+
+	out = append(out, bottom)
+	return strings.Join(out, "\n")
+}
